f6138e5901c94aec1cb81f4c937991fbc86e5f92
Sahil-Raj75 <sraj90015@gmail.com>
2024-08-29 14:24:08 +0530

    added home.html along with /home endpoint

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def hello():
     return render_template('index.html')
 
+@app.route('/home')
+def show_home():
+    return render_template('home.html')
+
 if __name__ == "__main__":
     app.run(debug=True)
