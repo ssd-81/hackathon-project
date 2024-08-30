12b4418c1a0c0bd51769ecf8d4d3860fd7903c96
Singh-Sagar <arcsagar.91@gmail.com>
2024-08-31 00:14:59 +0530

    configured api endpoints

--- app.py
+++ app.py
@@ -1,6 +1,4 @@
-from flask import Flask, render_template , jsonify 
-import sqlalchemy
-from flask import url_for
+from flask import Flask, render_template , jsonify, url_for
 from database import load_records_from_db
 
 
@@ -9,7 +7,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return render_template('page1.html')
+    return render_template('back.html')
 
 
 @app.route('/entry')
@@ -21,9 +19,9 @@ def return_record():
     records = load_records_from_db()
     return render_template('records.html', records=records)
 
-@app.route('/home')
+@app.route('/login')
 def home():
-    return render_template('back.html')
+    return render_template('page1.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
