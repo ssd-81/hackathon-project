32fc5bfe3b584cd312880a70ca6e13886d9e626d
Sahil-Raj75 <sraj90015@gmail.com>
2024-08-30 11:55:07 +0530

    interlinking done

--- app.py
+++ app.py
@@ -1,33 +1,8 @@
 from flask import Flask, render_template , jsonify 
+from flask import url_for
 
 app = Flask(__name__)
 
-JOBS = [ # type: ignore
-    {
-        'id': 1,
-        'title':'Data analyst',
-        'location':'delhi,India',
-        'salary':'10,00,000',
-    },
-    {
-        'id': 2,
-        'title':'Data scientist',
-        'location':'bengluru,India',
-        'salary':'15,00,000',
-    },
-    {
-        'id': 3,
-        'title':'Frontend engineer.',
-        'location':'hydrabad,India',
-        'salary':'12,00,000',
-    },
-    {
-        'id': 4,
-        'title':'Backend engineer',
-        'location':'stanford,USA',
-        'salary':'$32,000',
-    },
-    ]
 
 @app.route('/')
 def hello():
